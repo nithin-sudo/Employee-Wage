314cac09fc2d54728fdd124e20a7734665bb6574
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-21 14:32:16 +0530

    Checking Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,15 @@
 package com.bridgelabz;
-
 public class EmployeeWage {
         public static void main(String[] args){
-            System.out.println("Welcome to Employee Computation Program");
+                int IS_FULL_TIME=1;
+                double random=Math.floor(Math.random()*10)%2;
+                if (random==IS_FULL_TIME)
+                {
+                    System.out.println("Employee is present");
+                }
+                else
+                {
+                    System.out.println("Employee is absent");
+                }
         }
 }
