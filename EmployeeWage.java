8c816c30373ab26bfa0ef7a683298bae25a315e9
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-21 14:44:17 +0530

    Calculating Employee Daily Wage by taking wage per hr and full day work hour

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,14 +2,19 @@ package com.bridgelabz;
 public class EmployeeWage {
         public static void main(String[] args){
                 int IS_FULL_TIME=1;
+                int WAGE_PER_HOUR=20;
+                int FULL_WORK_HOUR=8;
+                int dailyWage;
                 double random=Math.floor(Math.random()*10)%2;
                 if (random==IS_FULL_TIME)
                 {
-                    System.out.println("Employee is present");
+                        System.out.println("Employee is present");
+                        dailyWage=(WAGE_PER_HOUR*FULL_WORK_HOUR);
+                        System.out.println("Daily wage of an employee is:"+dailyWage);
                 }
                 else
                 {
-                    System.out.println("Employee is absent");
+                        System.out.println("Employee is absent for the day his salary is zero");
                 }
         }
 }
