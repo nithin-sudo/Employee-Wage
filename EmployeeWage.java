2dd25fec035bf45d1ffaafd9928de4c88bb2f489
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-22 19:15:38 +0530

    MODIFIED:Employee wage Class to check Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,9 +2,6 @@ package com.bridgelabz;
 public class EmployeeWage {
         public static void main(String[] args){
                 int IS_FULL_TIME=1;
-                /**
-                 * Generating a random number by using that we are checking employee attendance.
-                 */
                 double random=Math.floor(Math.random()*10)%2;
                 if (random==IS_FULL_TIME)
                 {
