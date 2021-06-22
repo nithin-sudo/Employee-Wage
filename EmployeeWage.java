5d5ea635c83b29f11b23a25faed424a5d1008f25
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-22 19:01:47 +0530

    MODIFIED:EmployeeWage class and checked employee attendace

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,35 +1,18 @@
 package com.bridgelabz;
 public class EmployeeWage {
-        static final int IS_FULL_TIME=1;
-        static final int IS_PART_PRESENT=2;
-        static int WAGE_PER_HOUR=20;
-        static int WORKING_DAYS=20;
         public static void main(String[] args){
-                int empHrs=0;
-                int dailyWage;
-                int totalWage = 0;
-                int totalWorkingHrs = 0;
-                for (int i=1;i<WORKING_DAYS;i++){
-                        double empCheck=Math.floor(Math.random()*10)%3;
-                        switch ((int)empCheck) {
-                                case IS_FULL_TIME:
-                                        empHrs=8;
-                                        break;
-                                case IS_PART_PRESENT:
-                                        empHrs=4;
-                                        break;
-                                default:
-                                        empHrs=0;
-                        }
-                        if (totalWorkingHrs==100 || i==20)
-                        {
-                                break;
-                        }
-                        dailyWage=(WAGE_PER_HOUR*empHrs);
-                        totalWage+=dailyWage;
-                        totalWorkingHrs+=empHrs;
+                int IS_FULL_TIME=1;
+                /**
+                 * Generating a random number by using that we are checking employee attendance.
+                 */
+                double random=Math.floor(Math.random()*10)%2;
+                if (random==IS_FULL_TIME)
+                {
+                        System.out.println("Employee is present");
                 }
-                System.out.println("Salary per month is:"+totalWage);
+                else
+                {
+                        System.out.println("Employee is absent");
+                }
         }
-
 }
